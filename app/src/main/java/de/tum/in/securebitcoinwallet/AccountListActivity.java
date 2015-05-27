0f7a551672ddc8ff23e16baefd50dccf878fc194
Hannes Dorfmann <hannes.dorfmann@gmail.com>
2015-05-27 16:49:23 +0200

    Initial commit

---
+++ app/src/main/java/de/tum/in/securebitcoinwallet/AccountListActivity.java
@@ -0,0 +1,12 @@
+package de.tum.in.securebitcoinwallet;
+
+import android.os.Bundle;
+
+public class AccountListActivity extends BaseActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_account_list);
+  }
+
+}
---
+++ app/src/main/java/de/tum/in/securebitcoinwallet/BaseActivity.java
@@ -0,0 +1,9 @@
+package de.tum.in.securebitcoinwallet;
+
+import com.hannesdorfmann.mosby.MosbyActivity;
+
+/**
+ * @author Hannes Dorfmann
+ */
+public class BaseActivity extends MosbyActivity {
+}
